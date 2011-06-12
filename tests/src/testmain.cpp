7714b4d18648966d2ba8552a565d010c70f32c50
Bernd Stramm <bernd.stramm@gmail.com>
2011-06-11 21:53:44 -0400

    test event entry

--- tests/src/testmain.cpp
+++ tests/src/testmain.cpp
@@ -1,10 +1,36 @@
+
+#include <iostream>
 #include <scheduler.h>
+#include <event.h>
+
+using namespace std;
+
+class MyEvent : public msim::Event {
+public:
+
+  MyEvent (msim::SimTime when);
+
+  void happen () ;
+};
+
+MyEvent::MyEvent (msim::SimTime when)
+  :Event (when)
+{
+}
+
+void
+MyEvent::happen ()
+{
+  cout << __PRETTY_FUNCTION__ << " is was time" << endl;
+}
 
 int
 main (int argc, char* argv[])
 {
   msim::Scheduler Sch;
   msim::SimTime  endTime (21);
+  MyEvent  myEvent (msim::SimTime (10));
+  Sch.addEvent (&myEvent);
   Sch.runUntil (endTime);
   return 0;
 }
